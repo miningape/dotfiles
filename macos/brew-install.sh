8f1e892334f579b811caa429333815ba57e47084
Kyle Johnson <kyle.johnson@brandwatch.com>
2025-10-04 18:51:52 +0200

    Update brew-install.sh

--- macos/brew-install.sh
+++ macos/brew-install.sh
@@ -2,3 +2,4 @@
 
 brew install fastfetch # https://github.com/fastfetch-cli/fastfetch
 brew install fzf # https://github.com/junegunn/fzf
+brew install neovim # https://github.com/neovim/neovim/blob/master/INSTALL.md#install-from-package
